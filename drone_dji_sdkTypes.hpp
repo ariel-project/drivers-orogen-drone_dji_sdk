a58b32905d595e86cf3504da317165c65a3de759
rodrigo <rodrigo.leonello@tidewise.io>
2021-12-21 13:41:28 -0300

    initial commit by rock-create-orogen

---
+++ drone_dji_sdkTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef drone_dji_sdk_TYPES_HPP
+#define drone_dji_sdk_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace drone_dji_sdk {
+}
+
+#endif
+
